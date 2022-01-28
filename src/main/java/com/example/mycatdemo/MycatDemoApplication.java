465902fb1dfaf0a8f70f9f0d36e9ebd23d4d211c
SIYUAN XU <xsiyuan@gmail.com>
2022-01-28 14:55:00 +0800

    mycat 自身事务处理演示

--- src/main/java/com/example/mycatdemo/MycatDemoApplication.java
+++ src/main/java/com/example/mycatdemo/MycatDemoApplication.java
@@ -1,10 +1,11 @@
 package com.example.mycatdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/** @author afu */
 @SpringBootApplication
+@MapperScan("com.example.mycatdemo.dao")
 public class MycatDemoApplication {
 
   public static void main(String[] args) {
--- src/test/java/com/example/mycatdemo/MycatDemoApplicationTests.java
+++ src/test/java/com/example/mycatdemo/MycatDemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.mycatdemo;
 
+import com.example.mycatdemo.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MycatDemoApplicationTests {
+public class MycatDemoApplicationTests {
+  @Autowired private UserService userService;
 
   @Test
-  void contextLoads() {
-  }
+  public void contextLoads() {}
 
+  @Test
+  public void testUser() {
+    userService.testUser();
+  }
 }
